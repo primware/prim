29cfa5fa9f51652cacae6ab4999850e1af67d4ec
Josian Ascanio <josianascanio@casadelsoftware.com>
2025-10-24 14:18:39 -0500

    refactor: remove unused total calculation from summary recalculation

--- lib/views/Home/order/my_order_new.dart
+++ lib/views/Home/order/my_order_new.dart
@@ -466,7 +466,6 @@ class _OrderNewPageState extends State<OrderNewPage> {
   void _recalculateSummary() {
     double newSubtotal = 0.0;
     double newIVA = 0.0;
-    double newTotal = 0.0;
 
     for (var line in invoiceLines) {
       final price = _r2(line['price'] ?? 0);
@@ -484,9 +483,6 @@ class _OrderNewPageState extends State<OrderNewPage> {
 
       final lineTax = _r2(lineNet * (taxPercent / 100));
       newIVA += lineTax;
-
-      final lineTotal = _r2(lineNet + lineTax);
-      newTotal += lineTotal;
     }
 
     setState(() {
